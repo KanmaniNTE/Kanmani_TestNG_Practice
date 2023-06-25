b67f79c65ddf34bb692b340de81f93fe5ee35025
KanmaniNTE <nte.kanmani31@gmail.com>
2023-06-25 22:23:43 +0530

    Common Pages Elements

--- src/test/java/com/orangehrm/pages/CommonPage.java
+++ src/test/java/com/orangehrm/pages/CommonPage.java
@@ -2,6 +2,7 @@ package com.orangehrm.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 
 public class CommonPage {
@@ -15,5 +16,49 @@ public class CommonPage {
 	public void verifyAPageByWebElement(WebElement ele, String text) {
 		Assert.assertEquals(ele.getText(), text);
 	}
+	
+	@FindBy(xpath = "//div[contains(@class,'header-title')]")
+	WebElement header_Title;
+	
+	@FindBy(xpath = "//div[contains(@class,'search')]")
+	WebElement textBox_Search;
+	
+	@FindBy(xpath = "//a[contains(@href,'admin')]")
+	WebElement link_Admin;
+	
+	@FindBy(xpath = "//a[contains(@href,'viewPimModule')]")
+	WebElement link_PIM ;
+	
+	@FindBy(xpath = "//a[contains(@href,'leave')]")
+	WebElement link_Leave;
+	
+	@FindBy(xpath = "//a[contains(@href,'Time')]")
+	WebElement link_Time;
+	
+	@FindBy(xpath = "//a[contains(@href,'Recruitment')]")
+	WebElement link_Recruitment;
+	
+	@FindBy(xpath = "//a[contains(@href,'viewMyDetails')]")
+	WebElement link_MyInfo;
+	
+	@FindBy(xpath = "//a[contains(@href,'Performance')]")
+	WebElement link_Performance ;
+	
+	@FindBy(xpath = "//a[contains(@href,'dashboard')]")
+	WebElement link_Dashboard;
+	
+	@FindBy(xpath = "//a[contains(@href,'directory')]")
+	WebElement link_Directory;
+	
+	@FindBy(xpath = "//a[contains(@href,'maintenance')]")
+	WebElement link_Maintenance;
+	
+	@FindBy(xpath = "//a[contains(@href,'buzz')]")
+	WebElement link_Buzz;
+	
+//	@FindBy()
+//	WebElement ;
+//	@FindBy()
+//	WebElement ;
 
 }
--- src/test/java/com/orangehrm/pages/PIMPage.java
+++ src/test/java/com/orangehrm/pages/PIMPage.java
@@ -1,5 +1,73 @@
 package com.orangehrm.pages;
 
-public class PIMPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
+public class PIMPage {
+WebDriver driver;
+	
+	
+	public void verifyAPageByTitle(String title) {
+		Assert.assertEquals(driver.getTitle(), title);
+	}
+	
+	public void verifyAPageByWebElement(WebElement ele, String text) {
+		Assert.assertEquals(ele.getText(), text);
+	}
+	
+	@FindBy(xpath = "//div[contains(@class,'oxd-grid-item oxd-grid-item--gutters')][1]")
+	WebElement textBox_EmployeeName;
+	
+	@FindBy(xpath = "//div[contains(@class,'oxd-grid-item oxd-grid-item--gutters')][2]")
+	WebElement textBox_EmployeeID;
+	
+	@FindBy(xpath = "//div[contains(@class,'oxd-grid-item oxd-grid-item--gutters')][3]")
+	WebElement dropDown_EmployeeStatus;
+	
+	@FindBy(xpath = "//div[contains(@class,'oxd-grid-item oxd-grid-item--gutters')][4]")
+	WebElement dropDown_Include;
+	
+	@FindBy(xpath = "//div[contains(@class,'oxd-grid-item oxd-grid-item--gutters')][5]")
+	WebElement textBox_SupervisorName;
+	
+	@FindBy(xpath ="//div[contains(@class,'oxd-grid-item oxd-grid-item--gutters')][6]")
+	WebElement dropDown_JobTitle;
+	
+	@FindBy(xpath ="//div[contains(@class,'oxd-grid-item oxd-grid-item--gutters')][7]")
+	WebElement dropDown_SubUnit;
+	
+	@FindBy(xpath ="//li[contains(@class,'tab --parent')]")
+	WebElement list_Configuration;
+	
+	@FindBy(xpath = "//a[contains(text(),'Employee List')]")
+	WebElement link_EmployeeList;
+	
+	@FindBy(xpath = "//a[contains(text(),'Add Employee')]")
+	WebElement link_AddEmployee;
+	
+	@FindBy(xpath = "//a[contains(text(),'Reports')]")
+	WebElement link_Reports ;
+	
+	@FindBy(xpath = "//button[@title='Help']")
+	WebElement button_Help;
+	
+	@FindBy(xpath = "//button[@type='reset']")
+	WebElement button_Reset;
+	
+	@FindBy(xpath = "//button[@type='submit']")
+	WebElement button_Search;
+	
+	@FindBy(xpath = "//button[contains(@class,'button--secondary')][2]")//not working
+	WebElement button_Add;
+	
+	@FindBy()
+	WebElement ;
+	@FindBy()
+	WebElement ;
+	@FindBy()
+	WebElement ;
+	@FindBy()
+	WebElement ;
 }
